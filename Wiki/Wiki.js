4a3b42a20ca55385296a6a7531405a531dab290e
SheeranX <stevehardiman@hotmail.com>
2017-08-29 22:14:57 +0800

    update wiki

--- Wiki/Wiki.js
+++ Wiki/Wiki.js
@@ -1,20 +1,25 @@
 // JavaScript source code
-var app = angular.module("app", ['ui.router']);
-app.config(["$stateProvider",'$urlRouterProvider',function($stateProvider,$urlRouterProvider){
-	$urlRouterProvider.when('','/Search');
-	$stateProvider
-	.state('Search',{
-	   url:'/Search',
-       templateUrl:'Search.html'	   
-	})
-	.state("Result",{
-		url:'/Result',
-		templateUrl:'Result.html'
-	});
-}]);
+var app = angular.module("app", []);
+// app.config(["$stateProvider",'$urlRouterProvider',function($stateProvider,$urlRouterProvider){
+// 	$urlRouterProvider.when('','/Search');
+// 	$stateProvider
+// 	.state('Search',{
+// 	   url:'/Search',
+//        templateUrl:'Search.html'	   
+// 	})
+// 	.state("Result",{
+// 		url:'/Result',
+// 		templateUrl:'Result.html'
+// 	});
+// }]);
  app.controller("ctrl", ["$scope", "$http", function ($scope, $http) {
+
+
      $scope.iconSearch = function () {
-         $http.get("").then();
-         alert($scope.txtContent);
+       if ($scope.txtContent==""||$scope.txtContent==null) {
+ 	 	     
+ 	   }
+
+        alert($scope.txtContent);
      }
  }]);
