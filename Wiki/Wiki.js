7eab646ffa8635044038abd2cc9eb7216becb4c3
Sheeran Xu <stevehardiman@hotmail.com>
2017-08-31 15:22:57 +0800

    Update Wiki.js

--- Wiki/Wiki.js
+++ Wiki/Wiki.js
@@ -12,14 +12,39 @@ var app = angular.module("app", []);
 // 		templateUrl:'Result.html'
 // 	});
 // }]);
+app.factory('myIntercepter',['$rootScope',function($rootScope){ 
+ return { 
+  request:function(config){  
+    $rootScope.load = false;  
+    $rootScope.resultShow = true; 
+    return config; 
+  },  
+  response:function(response){ 
+   $rootScope.load = true; 
+   $rootScope.resultShow = false; 
+   return response;  
+  }
+ }
+}]);
+app.config(["$httpProvider",function($httpProvider){ $httpProvider.interceptors.push("myIntercepter");}]);
  app.controller("ctrl", ["$scope", "$http", function ($scope, $http) {
+      $rootScope.load = true; 
+      $scope.n = name; 
+      $rootScope.resultShow = false;   
+      $scope.iconSearch = function () {     
+       if ($scope.txtContent==""||$scope.txtContent==null) {    
+        return false;  
+       }else{  
+        $http.post("http://en.wikipedia.org/w/api.php?action=query&list=search&srwhat=text&srsearch="+$scope.txtContent+"&format=json").then(function(response){ 
+         $scope.result =response.data;  
+         //$scope.content =$sce.trustAsHtml(response.data.query.search);  
+         //alert($scope.txtContent); 
+        });  
+       }    
+      }   
+      $scope.convert = function(n){  
+       return $sce.trustAsHtml(n+"..."); 
+      }
 
-
-     $scope.iconSearch = function () {
-       if ($scope.txtContent==""||$scope.txtContent==null) {
- 	 	     
- 	   }
-
-        alert($scope.txtContent);
-     }
+         
  }]);
