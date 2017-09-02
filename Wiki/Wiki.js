e7b337e985acf7d3c312d8bcc4b1e6fdc8310d26
SheeranX <stevehardiman@hotmail.com>
2017-09-02 11:53:55 +0800

    update wiki

--- Wiki/Wiki.js
+++ Wiki/Wiki.js
@@ -1,50 +1,54 @@
 // JavaScript source code
 var app = angular.module("app", []);
 // app.config(["$stateProvider",'$urlRouterProvider',function($stateProvider,$urlRouterProvider){
-// 	$urlRouterProvider.when('','/Search');
-// 	$stateProvider
-// 	.state('Search',{
-// 	   url:'/Search',
-//        templateUrl:'Search.html'	   
-// 	})
-// 	.state("Result",{
-// 		url:'/Result',
-// 		templateUrl:'Result.html'
-// 	});
+//  $urlRouterProvider.when('','/Search');
+//  $stateProvider
+//  .state('Search',{
+//     url:'/Search',
+//        templateUrl:'Search.html'    
+//  })
+//  .state("Result",{
+//    url:'/Result',
+//    templateUrl:'Result.html'
+//  });
 // }]);
-app.factory('myIntercepter',['$rootScope',function($rootScope){ 
- return { 
-  request:function(config){  
-    $rootScope.load = false;  
-    $rootScope.resultShow = true; 
-    return config; 
-  },  
-  response:function(response){ 
-   $rootScope.load = true; 
-   $rootScope.resultShow = false; 
-   return response;  
-  }
- }
+app.factory('myIntercepter',['$rootScope',function($rootScope){
+  return {
+    request:function(config){
+      $rootScope.load = false;
+      $rootScope.resultShow = true;
+      return config;
+    },
+    response:function(response){
+      $rootScope.load = true;
+      $rootScope.resultShow = false;
+      return response;
+}
+}
 }]);
-app.config(["$httpProvider",function($httpProvider){ $httpProvider.interceptors.push("myIntercepter");}]);
- app.controller("ctrl", ["$scope", "$http", function ($scope, $http) {
-      $rootScope.load = true; 
-      $scope.n = name; 
-      $rootScope.resultShow = false;   
-      $scope.iconSearch = function () {     
-       if ($scope.txtContent==""||$scope.txtContent==null) {    
-        return false;  
-       }else{  
-        $http.post("http://en.wikipedia.org/w/api.php?action=query&list=search&srwhat=text&srsearch="+$scope.txtContent+"&format=json").then(function(response){ 
-         $scope.result =response.data;  
-         //$scope.content =$sce.trustAsHtml(response.data.query.search);  
-         //alert($scope.txtContent); 
-        });  
-       }    
-      }   
-      $scope.convert = function(n){  
-       return $sce.trustAsHtml(n+"..."); 
-      }
+app.config(["$httpProvider",function($httpProvider){ $httpProvider.interceptors.push("myIntercepter");
+}]);
+app.controller("ctrl", ["$scope", "$http","$rootScope","$sce", function ($scope, $http,$rootScope,$sce) {
+  $rootScope.load = true;
+  $scope.n = name;
+  $rootScope.resultShow = false;
+  //  $headers = {
+  //     'Access-Control-Allow-Origin': '*',
+  //     'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT',
+  //     'Content-Type': 'application/application/x-www-form-urlencoded',
+  //     'Accept': 'application/json'
+  // };
+ // $scope.postUrl = $sce.trustAsUrl("http://en.wikipedia.org/w/api.php?action=query&list=search&srwhat=text&srsearch="+$scope.txtContent+"&format=json");
+  $scope.iconSearch = function () {
+    if ($scope.txtContent==""||$scope.txtContent==null)   
+      {
+        return false;
+      }else{
+        $scope.trustUrl = $sce.trustAsResourceUrl("http://en.wikipedia.org/w/api.php?action=query&list=search&srwhat=text&srsearch="+$scope.txtContent+"&format=json");
 
-         
- }]);
+        $http.jsonp($scope.trustUrl).then(function(response){
+          $scope.result =response.data;
+        });
+      }}
+      $scope.convert = function(n){return $sce.trustAsHtml(n+"...");}
+}]);
