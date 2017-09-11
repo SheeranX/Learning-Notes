761c7f2ef0979faa2e625728985875848bafb594
SheeranX <stevehardiman@hotmail.com>
2017-09-11 21:58:49 +0800

    update cart

--- AngularJSDemo/ShoppingCart/Cart.js
+++ AngularJSDemo/ShoppingCart/Cart.js
@@ -1,5 +1,40 @@
 //alert("---");
 angular.module('app', [])
 	   .controller('ctrl', ['$scope', function($scope){
-	   	  
+	   	  $scope.goods=[
+	   	  	{id:'g0001',name:"Sheeran",num:2,imgUrl:'taylor_1.jpg',description:'the singer song-writer',price:12},
+	   	  	{id:'g0002',name:"Taylor",num:5,imgUrl:'taylor_1.jpg',description:'my favorite female singer',price:13},
+	   	  	{id:'g0003',name:"Coldplay",num:4,imgUrl:'taylor_1.jpg',description:'Not a band but artist',price:14}
+	   	  ];
+
+          $scope.plus = function(id){
+          	angular.forEach($scope.goods,function(item,index,array){
+          		if(item.id===id)
+          			item.num++;
+          	});
+          };
+
+          $scope.minus = function(id){
+          	angular.forEach($scope.goods, function(item,index, array){
+          		if(item.id===id)
+          			item.num--;
+          	});
+          };
+
+          $scope.total = function(){
+          	var sum = 0;
+          	angular.forEach($scope.goods, function(item, index,array){
+          		sum+=item.num*item.price;
+          	});
+          	return sum;
+          };
+
+          
+
+
+
+
+
+
+
 	   }]);
