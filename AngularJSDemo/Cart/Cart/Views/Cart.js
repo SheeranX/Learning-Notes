6effa6a2d84134dd36cae0bfebb47098c2936787
SheeranX <stevehardiman@hotmail.com>
2017-09-13 22:42:19 +0800

    update cart

--- AngularJSDemo/Cart/Cart/Views/Cart.js
+++ AngularJSDemo/Cart/Cart/Views/Cart.js
@@ -2,6 +2,7 @@
 angular.module('app', [])
 	   .controller('ctrl', ['$scope', '$http', function ($scope, $http) {
 	       $scope.goods = {};
+	       $scope.chooseArr = [];
 	   	  //$scope.goods=[
 	   	  //	{id:'g0001',name:"Sheeran",num:2,imgUrl:'taylor_1.jpg',description:'the singer song-writer',price:12},
 	   	  //	{id:'g0002',name:"Taylor",num:5,imgUrl:'taylor_1.jpg',description:'my favorite female singer',price:13},
@@ -18,10 +19,12 @@ angular.module('app', [])
 	           for (var i = 0; i < $scope.goods.length; i++) {
 	               var item = $scope.goods[i];
 	               $scope.total += item.Quantity * item.Price;
+
 	           }
-	       }
+	       };
 
 	       $scope.plus = function (index) {
+	           $scope.total = 0;
 	           $scope.goods[index].Quantity++;
 	           getTotal();
           };
@@ -30,12 +33,50 @@ angular.module('app', [])
 	           if ($scope.goods[index].Quantity < 2)
 	               return;
 	           else {
+	               $scope.total = 0;
 	               $scope.goods[index].Quantity--;
 	               getTotal();
 	           }
                    
-          };
+	       };
 
+	       $scope.delItem = function (index) {
+	           $scope.goods.splice(index,1);   
+	       };
+
+	       $scope.delAll = function () {
+	           if ($scope.all)
+	           {
+	               $scope.goods.splice(0, $scope.goods.length);
+	               $scope.total = 0;
+	           }
+	       };
+
+	       $scope.selected = function (no, id) {
+	           if (no)
+	               forArr(id);
+	           else {
+	               $scope.chooseArr.remove(id);
+	           }
+	       };
+
+	       function forArr(id) {
+	           angular.forEach($scope.chooseArr, function (item, index, array) {
+	               if (item.Id == id)
+	                   return;
+	               else
+	               {
+	                   $scope.chooseArr.push(id);
+	               }
+	           });
+	       }
+
+	       Array.prototype.remove = function (val) {
+	           var index = this.indexOf(val);
+	           if (index > -1) {
+	               this.splice(index, 1);
+	           }
+	       };
           //$scope.total = function(){
           //	var sum = 0;
           //	angular.forEach($scope.goods, function(item, index,array){
