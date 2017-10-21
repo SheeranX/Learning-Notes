2589c0a373efee78010b788a895bf65f54bfae3d
SheeranX <stevehardiman@hotmail.com>
2017-10-21 18:48:21 +0800

    update shoppingcart

--- AngularJSDemo/ShoppingCart/Cart.js
+++ AngularJSDemo/ShoppingCart/Cart.js
@@ -1,40 +1,105 @@
 //alert("---");
-angular.module('app', [])
-	   .controller('ctrl', ['$scope', function($scope){
-	   	  $scope.goods=[
-	   	  	{id:'g0001',name:"Sheeran",num:2,imgUrl:'taylor_1.jpg',description:'the singer song-writer',price:12},
-	   	  	{id:'g0002',name:"Taylor",num:5,imgUrl:'taylor_1.jpg',description:'my favorite female singer',price:13},
-	   	  	{id:'g0003',name:"Coldplay",num:4,imgUrl:'taylor_1.jpg',description:'Not a band but artist',price:14}
-	   	  ];
+ var app = angular.module('app', []);
+     app.controller('ctrl', ctrl);
+     //app.filter('numFilter',numFilter);
 
-          $scope.plus = function(id){
-          	angular.forEach($scope.goods,function(item,index,array){
+     // function numFilter(){
+     //    return function(val){
+     //      if(val<1)
+     //        return 1;
+     //    }
+     // }
+     var item = [
+             {id:'g0001',name:"Sheeran",num:2,imgUrl:'taylor_1.jpg',description:'the singer song-writer',price:12},
+             {id:'g0002',name:"Taylor",num:5,imgUrl:'taylor_1.jpg',description:'my favorite female singer',price:13},
+             {id:'g0003',name:"Coldplay",num:4,imgUrl:'taylor_1.jpg',description:'Not a band but artist',price:14}
+            ];
+     function ctrl(){
+
+           var vm = this;
+
+           vm.goods="";
+
+           vm.goods = item;
+           vm.flag = false;
+           vm.arr = [];
+
+           vm.plus = plus;
+           vm.minus = minus;
+           vm.total = total;
+           vm.delItems = delItems;
+           vm.pay = pay;
+           vm.selectItems = selectItems;
+	   	     
+
+          // vm.$watch('val',function(newVal){
+          //    vm.val  = $filter('numFilter')(newVal);
+          // });
+
+          function plus(id){
+          	angular.forEach(vm.goods,function(item,index,array){
           		if(item.id===id)
-          			item.num++;
+          		   item.num++;
           	});
           };
 
-          $scope.minus = function(id){
-          	angular.forEach($scope.goods, function(item,index, array){
+          function minus(id){
+          	angular.forEach(vm.goods, function(item,index, array){
           		if(item.id===id)
-          			item.num--;
+              {
+                if(item.num<2)
+                  return;
+                else
+          			 item.num--;
+              }
           	});
           };
 
-          $scope.total = function(){
+          function total(){
           	var sum = 0;
-          	angular.forEach($scope.goods, function(item, index,array){
+
+            if(!vm.all)
+              return sum = 0;
+            if(vm.arr.length>0)
+            {
+              for(var i = 0;i<vm.arr.length;i++)
+              {
+                sum += (vm.goods[vm.arr[i]].price)*(vm.goods[vm.arr[i]].num);
+                return sum;
+              }
+            }
+          	angular.forEach(vm.goods, function(item, index,array){
           		sum+=item.num*item.price;
           	});
           	return sum;
           };
 
+          function delItems(index){
+              vm.goods.splice(index,1);
+          }
+
+          function pay(){
+            alert(vm.total());
+          }
+       
+          function selectItems(id){
+            if(!vm.falg)
+            {
+              
+            }
+              if(vm.arr.length>0)
+              {
+                for(var i = 0;i<vm.arr.length;i++)
+                {
+                  if(vm.arr[i]==id)
+                    return;
+                  else
+                    vm.arr.push(id);
+                }
+              }
+              else
+                vm.arr.push(id);
+            console.log(vm.arr);
+          }
           
-
-
-
-
-
-
-
-	   }]);
+	   };
